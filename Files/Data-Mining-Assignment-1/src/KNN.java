5e7cbed46eafbca4633560ee3f7453b0442db91d
jmayn001 <jmayn001@gold.ac.uk>
2017-12-09 18:56:41 +0000

    TRAINING AND TESTING WORKS!!!!
    Report function added and data parsing all in Strings

--- Files/Data-Mining-Assignment-1/src/KNN.java
+++ Files/Data-Mining-Assignment-1/src/KNN.java
@@ -4,13 +4,31 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static java.lang.Math.pow;
+
 public class KNN {
 
 
-    public static double euclideanDist(int x1, int x2, int y1, int y2) {
-        double euclidean = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+    public static void report(String name, int num, ArrayList<String> att1, ArrayList<String> att2, ArrayList<String> att3) {
+        //Print Test of Training and Testing data, only displays 3 attributes - to test if ProcessData Works
+        System.out.println(name);
+        for (int i = 0; i < att1.size() && i < num; i++) {
+            System.out.println(
+                    i + ".\t\t" +
+                            att1.get(i)
+                            + "\t" +
+                            att2.get(i)
+                            + "\t" +
+                            att3.get(i)
+            );
+        }
+        System.out.println();
+    }
 
-        return euclidean;
+    public static double euclideanDist(int x1, int x2, int y1, int y2) {
+        double euclidean;
+
+        return euclidean = Math.sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
     }
 
     public static void main(String[] args) {
@@ -20,62 +38,60 @@ public class KNN {
             BufferedReader readTestData = new BufferedReader(new FileReader("test.csv"));
 
             try {
-                // readTrainData.readLine();
+
+                //TRAINING DATA
+                String line;
+//                readTrainData.readLine();
                 ArrayList<String> trainEducation = new ArrayList<String>();
                 ArrayList<String> trainGender = new ArrayList<String>();
                 ArrayList<String> trainEarns = new ArrayList<String>();
-
-                String line;
-
                 readTrainData.readLine();
                 while ((line = readTrainData.readLine()) != null) {
                     String[] columnSplit = line.split(",");
-                    trainEducation.add((columnSplit[0]));
-                    trainGender.add((columnSplit[1]));
-                    trainEarns.add((columnSplit[2]));
+//                    trainEducation.add(Integer.parseInt(columnSplit[0].trim()));
+//                    trainGender.add(Integer.parseInt(columnSplit[1].trim()));
+//                    trainEarns.add(Integer.parseInt(columnSplit[2].trim()));
+                    trainEducation.add((columnSplit[0].trim()));
+                    trainGender.add((columnSplit[1].trim()));
+                    trainEarns.add((columnSplit[2].trim()));
 
                 }
+                report("TrainData", 20, trainEducation, trainGender, trainEarns);
 
 
-                ArrayList<Integer> testEducation = new ArrayList<Integer>();
-                ArrayList<Integer> testGender = new ArrayList<Integer>();
-                ArrayList<Integer> testEarns = new ArrayList<Integer>();
-
-                //String line;
+                String testLine;
+//                readTestData.readLine();
+                ArrayList<String> testEducation = new ArrayList<String>();
+                ArrayList<String> testGender = new ArrayList<String>();
+                ArrayList<String> testEarns = new ArrayList<String>();
                 readTestData.readLine();
-                while ((line = readTestData.readLine()) != null) {
-                    String[] columnSplit = line.split(",");
-                    testEducation.add(Integer.parseInt(columnSplit[0].trim()));
-                    testGender.add(Integer.parseInt(columnSplit[1].trim()));
-                    testEarns.add(Integer.parseInt(columnSplit[2].trim()));
-
+                while ((testLine = readTestData.readLine()) != null) {
+                    String[] columnSplit = testLine.split(",");
+                    testEducation.add((columnSplit[0].trim()));
+                    testGender.add((columnSplit[1].trim()));
+                    testEarns.add((columnSplit[2].trim()));
                 }
+                report("TestData", 20, testEducation, testGender, testEarns);
 
-                //euclideanDist(trainEducation.get(0), testEducation.get(0), trainGender.get(0), testGender.get(0));
-
-                for (int i = 0; i < /*testEducation.size()*/ 40; i++) {
-                    System.out.println(
-                            testEducation.get(i) +
-                                    "\t" +
-                                    testGender.get(i) +
-                                    "\t" +
-                                    testEarns.get(i)
-                    );
-                }
+//            for (int i = 0; i < 10; i++) {
+//                    euclideanDist(trainEducation.get(i), testEducation.get(i), trainGender.get(i), testGender.get(i));
+//            }
 
 
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
+
             }
         } catch (
                 FileNotFoundException e)
 
         {
+
             // TODO Auto-generated catch block
             e.printStackTrace();
+
+
         }
-
     }
-
 }
